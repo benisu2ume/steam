cef2343e378418f582f2089ba033848816f31d6c
benisu2ume <jens.o.morrison@gmail.com>
2019-03-24 19:33:00 -0700

    adding data files

--- plot.py
+++ plot.py
@@ -1,11 +1,31 @@
 #!/usr/bin/python
-
-print('here is my plot')
+# import math plotting library 
 import matplotlib.pyplot as plt
-x = [1,2,3]
-y = [2,4,2]
+# create two empty arrays or lists
+x = []
+y = []
+# open file and read it line by line
+with open('data/cu.data.1.AllItems.M01.CUSR0000SA0', 'r') as cpi_file:
+	# for each line in the file
+    for line in cpi_file:
+    	# split each line into an array by spaces
+    	d = line.split()
+    	# year is the 2nd element or index 1
+    	year = float(d[1])
+    	# cpi is the 4th element or index 3
+    	cpi = float(d[3])
+    	x.append(year)
+    	y.append(cpi)
+
+# plot x and y arrays
 plt.plot(x, y)
-plt.xlabel('x - axis')
-plt.ylabel('y - axis')
-plt.title('My first graph is fun!')
+# add xlabel
+plt.xlabel('year')
+# add ylabel
+plt.ylabel('CPI')
+# add a title
+plt.title('CPI from year 1947')
+# add grid lines
+plt.grid(True)
+# show the graph!
 plt.show()
